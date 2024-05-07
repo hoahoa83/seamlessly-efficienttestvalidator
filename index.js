2f6691864d105a0a785f50fd056e72771040adc6
sfasfafasfa <js1fabra502@hotmail.com>
2024-05-08 02:52:43 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
-};
+function wiggleMaxLength(nums) {
+  if (nums.length === 0) return 0;
+  let up = 1;
+  let down = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1;
+    } else if (nums[i] < nums[i - 1]) {
+      down = up + 1;
+    }
+  }
+  return Math.max(up, down);
+}
