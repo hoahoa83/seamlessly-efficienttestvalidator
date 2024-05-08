a429c531798ae9e6177d6717913c054ae0c8caaf
sfasfafasfa <js1fabra502@hotmail.com>
2024-05-08 19:27:39 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-function wiggleMaxLength(nums) {
-  if (nums.length === 0) return 0;
-  let up = 1;
-  let down = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) {
-      up = down + 1;
-    } else if (nums[i] < nums[i - 1]) {
-      down = up + 1;
-    }
-  }
-  return Math.max(up, down);
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
 }
